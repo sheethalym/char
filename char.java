04ffc7b9319fd250424b2bb7fda35a2c26cce79d
sheethalym <sheethal.xworkz@gmail.com>
2021-12-20 21:46:58 +0530

    adding char program

---
+++ char.java
@@ -0,0 +1,17 @@
+class Char{
+ public static void main(String[]args){
+  char symbol[]={'@','#','$','%','&','*','~','[',']','^'};
+  System.out.println(symbol[0]);
+ System.out.println(symbol[1]);
+ System.out.println(symbol[2]);
+ System.out.println(symbol[3]);
+ System.out.println(symbol[4]);
+ System.out.println(symbol[5]);
+ System.out.println(symbol[6]);
+ System.out.println(symbol[7]);
+ System.out.println(symbol[8]);
+ System.out.println(symbol[9]);
+
+ }
+
+}
